07a6da9428d0c50bd8fcb8bf0d9485d7cd90b129
Taisiya Nesterenko <prostotaya@gmail.com>
2024-08-19 15:46:10 +0300

    First commit from Intellij

---
+++ src/main/java/demo/MainClass.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
